0664e56b4ba451a43acd10b03277bdfa2ac5ca56
Ricardo Santana <ricardo.caduh001@gmail.com>
2022-02-15 00:20:55 -0300

    feat: ajuste na chamada da detailView atravez do didSelectRowAt

--- HarryPotter/View/Main/ViewController.swift
+++ HarryPotter/View/Main/ViewController.swift
@@ -37,25 +37,23 @@ class ViewController: UIViewController {
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.characters.count
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
         let character = viewModel.characters[indexPath.row]
-        
         cell.textLabel?.text = character.name
         cell.detailTextLabel?.text = character.house
         
         return cell
     }
     
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.characters.count
-    }
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let storyBoard = UIStoryboard(name: "Detail", bundle: nil)
-        guard let detailVC = storyboard?.instantiateViewController(withIdentifier: "detailVC") as? DetailViewController else { return }
+        guard let detailVC = storyBoard.instantiateViewController(withIdentifier: "detailVC") as? DetailViewController else { return }
         detailVC.character = viewModel.characters[indexPath.row]
         self.present(detailVC, animated: true, completion: nil)
     }
